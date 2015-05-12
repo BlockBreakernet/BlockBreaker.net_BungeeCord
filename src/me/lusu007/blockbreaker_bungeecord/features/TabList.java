9c081bcb533c8a08d993f18579b493f1b29a1132
lusu007 <lukas.jost@outlook.com>
2015-05-12 20:46:02 +0200

    TabList Bug

--- src/me/lusu007/blockbreaker_bungeecord/features/TabList.java
+++ src/me/lusu007/blockbreaker_bungeecord/features/TabList.java
@@ -3,7 +3,7 @@ package me.lusu007.blockbreaker_bungeecord.features;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
-import net.md_5.bungee.api.event.ServerConnectEvent;
+import net.md_5.bungee.api.event.ServerConnectedEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
@@ -18,20 +18,20 @@ public class TabList implements Listener {
     }
 
     @EventHandler
-    public void onConnect(ServerConnectEvent e) {
-        if(e.getTarget().getName().contentEquals("lobby")) {
+    public void onSwitch(ServerConnectedEvent e) {
+        if(e.getServer().getInfo().getName().contentEquals("lobby")) {
             e.getPlayer().setTabHeader(new TextComponent(ChatColor.AQUA + "        BlockBreaker" + ChatColor.GREEN + ".de        \n" +
                     ChatColor.GOLD + "Lobby"), new TextComponent("Änderbar"));
-        } else if(e.getTarget().getName().contentEquals("sw")) {
+        } else if(e.getServer().getInfo().getName().contentEquals("sw")) {
             e.getPlayer().setTabHeader(new TextComponent(ChatColor.AQUA + "        BlockBreaker" + ChatColor.GREEN + ".de        \n" +
                     ChatColor.AQUA + "SkyWars"), new TextComponent("Änderbar"));
-        } else if(e.getTarget().getName().contentEquals("sg")) {
+        } else if(e.getServer().getInfo().getName().contentEquals("sg")) {
             e.getPlayer().setTabHeader(new TextComponent(ChatColor.AQUA + "        BlockBreaker" + ChatColor.GREEN + ".de        \n" +
                     ChatColor.AQUA + "SurvivalGames"), new TextComponent("Änderbar"));
-        } else if(e.getTarget().getName().contentEquals("rpg")) {
+        } else if(e.getServer().getInfo().getName().contentEquals("rpg")) {
             e.getPlayer().setTabHeader(new TextComponent(ChatColor.AQUA + "        BlockBreaker" + ChatColor.GREEN + ".de        \n" +
                     ChatColor.AQUA + "RPG"), new TextComponent("Änderbar"));
-        } else if(e.getTarget().getName().contentEquals("stc")) {
+        } else if(e.getServer().getInfo().getName().contentEquals("stc")) {
             e.getPlayer().setTabHeader(new TextComponent(ChatColor.AQUA + "        BlockBreaker" + ChatColor.GREEN + ".de        \n" +
                     ChatColor.AQUA + "SafeTheCake"), new TextComponent("Änderbar"));
         }
